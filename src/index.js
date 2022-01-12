defca3a78a32582cc717af42de7f32ed810f820a
yazhao.hou <yazhao.hou@upai.com>
2022-01-12 11:07:37 +0800

    🎉 init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// interface SearchFunc {
+//   (source: string, subString: string): boolean
+// }
+var greet = function (name, age) {
+    return "Hello ".concat(name, ", your age is ").concat(age, ".");
+};
+console.log(greet('Aaron', 1));
